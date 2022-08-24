d4767da39bb6579d65737191eb72854d28c6e6b6
teeratchapat <teeratchapat.c@kkumail.com>
2022-08-24 15:43:01 +0700

    edit

--- src/views/Home.js
+++ src/views/Home.js
@@ -3,9 +3,7 @@ import React from 'react';
 import Hero from '../components/sections/Hero';
 import FeaturesTiles from '../components/sections/FeaturesTiles';
 import FeaturesSplit from '../components/sections/FeaturesSplit';
-import FeaturesSplit2 from '../components/sections/FeaturesSplit2';
-import FeaturesSplit3 from '../components/sections/FeaturesSplit3';
-import Testimonial from '../components/sections/Testimonial';
+// import Testimonial from '../components/sections/Testimonial';
 import Cta from '../components/sections/Cta';
 
 const Home = () => {
@@ -13,9 +11,7 @@ const Home = () => {
   return (
     <>
       <Hero className="illustration-section-01" />
-      {/* <FeaturesSplit2 invertMobile topDivider imageFill className="illustration-section-02" /> */}
       <FeaturesTiles />
-      {/* <FeaturesSplit3 invertMobile topDivider imageFill className="illustration-section-02" /> */}
       <FeaturesSplit invertMobile topDivider imageFill className="illustration-section-02" />
       {/* <Testimonial topDivider /> */}
       <Cta split />
